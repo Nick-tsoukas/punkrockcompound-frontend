62edfcf714bf6ef8531079837738b63c75e31b4e
Nikolaos Tsoukas <nikolaostsoukas@nikolaoss-imac.attlocal.net>
2022-04-14 23:22:59 -0500

    chore: adding band admin page

--- pages/BandAdmin.vue
+++ pages/BandAdmin.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    <h1>This is the band Admin</h1>
-    <p>This is band</p>
+    <section v-if="band">
+      <h2 class="main_red_text text-center pb-10">Band Details</h2>
+      <div
+        class="h-96 object-fill para relative"
+        :style="{
+          'background-image': `url(${band[0].bandProfileImg.url})`,
+        }"
+      >
+        <h2 class="main_red_text absolute bottom-10 left-10">
+          {{ band[0].bandName }}
+        </h2>
+      </div>
+      <section class="px-10"></section>
+    </section>
     <pre>{{ band }}</pre>
-    <p>This is user</p>
-    <pre>{{ $strapi.user }}</pre>
   </div>
 </template>
 
@@ -25,3 +35,13 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.para {
+  background-size: cover;
+  object-fit: fill;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
