c6f7960195848e2d3b06ee8af406c2255b2be527
Nikolaos Tsoukas <nikolaostsoukas@nikolaoss-imac.attlocal.net>
2022-04-15 11:57:40 -0500

    chore: design layout for admin page

--- pages/BandAdmin.vue
+++ pages/BandAdmin.vue
@@ -12,9 +12,102 @@
           {{ band[0].bandName }}
         </h2>
       </div>
-      <section class="px-10"></section>
+      <button
+        class="text-center text-white w-full py-4 px-4 bg-black shadow-md mt-10"
+      >
+        Edit Profile Image
+      </button>
+
+      <!-- Band Details Section -->
+      <section class="pl-10">
+        <h2 class="main_red_text text-center py-8">Band Details</h2>
+        <div class="md:flex">
+          <div class="pr-32">
+            <div class="pb-8">
+              <h3 class="main_red_text text-2xl">Admin Name</h3>
+              <div class="flex items-center">
+                <p class="text-xl pr-4">{{ adminName }}</p>
+                <img class="h-4" src="~/static/edit.svg" alt="" />
+              </div>
+            </div>
+            <div class="pb-8">
+              <h3 class="main_red_text text-2xl">Admin Email</h3>
+              <div class="flex items-center">
+                <p class="text-xl pr-4">
+                  {{ band[0].users_permissions_user.email }}
+                </p>
+                <img class="h-4" src="~/static/edit.svg" alt="" />
+              </div>
+            </div>
+            <div class="pb-8">
+              <h3 class="main_red_text text-2xl">Band Name</h3>
+              <div class="flex items-center">
+                <p class="text-xl pr-4">
+                  {{ band[0].bandName }}
+                </p>
+                <img class="h-4" src="~/static/edit.svg" alt="" />
+              </div>
+            </div>
+            <div class="pb-8">
+              <h3 class="main_red_text text-2xl">Genre</h3>
+              <div class="flex items-center">
+                <p class="text-xl pr-4">
+                  {{ band[0].genre }}
+                </p>
+                <img class="h-4" src="~/static/edit.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <!-- column two of details on medium -->
+          <div>
+            <div class="pb-8">
+              <h3 class="main_red_text text-2xl">Hometown</h3>
+              <div class="flex items-center">
+                <p class="text-xl pr-4">
+                  {{ hometown }}
+                </p>
+                <img class="h-4" src="~/static/edit.svg" alt="" />
+              </div>
+            </div>
+            <div class="pb-8">
+              <h3 class="main_red_text text-2xl">Record Label</h3>
+              <div class="flex items-center">
+                <p class="text-xl pr-4">
+                  {{ band[0].recordLabel }}
+                </p>
+                <img class="h-4" src="~/static/edit.svg" alt="" />
+              </div>
+            </div>
+            <div class="pb-8">
+              <h3 class="main_red_text text-2xl">Band Manager</h3>
+              <div class="flex items-center">
+                <p class="text-xl pr-4">
+                  {{ band[0].recordLabel }}
+                </p>
+                <img class="h-4" src="~/static/edit.svg" alt="" />
+              </div>
+            </div>
+            <div class="pb-8">
+              <h3 class="main_red_text text-2xl">Band Email</h3>
+              <div class="flex items-center">
+                <p class="text-xl pr-4">
+                  {{ band[0].bandEmail }}
+                </p>
+                <img class="h-4" src="~/static/edit.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div class="md:hidden lg:flex flex-col">
+            <h2>Add Albums</h2>
+            <h2>Add Albums</h2>
+            <h2>Add Albums</h2>
+            <h2>Add Albums</h2>
+          </div>
+        </div>
+      </section>
     </section>
-    <pre>{{ band }}</pre>
+
+    <!-- <pre>{{ band }}</pre> -->
   </div>
 </template>
 
@@ -26,6 +119,19 @@ export default {
       band: '',
     }
   },
+  computed: {
+    adminName() {
+      return (
+        this.band[0].users_permissions_user.firstName +
+        ' ' +
+        this.band[0].users_permissions_user.lastName
+      )
+    },
+    hometown() {
+      return this.band[0].city + ', ' + this.band[0].state
+    },
+  },
+
   async mounted() {
     if (this.$strapi.user) {
       this.band = await this.$strapi.find('bands', {
